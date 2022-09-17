35ae06bd6e10978649d225aad59e71a4408f5e8d
GodWin <shivamjpanchal1@gmail.com>
2022-09-18 05:14:32 +0530

    feat(test_utils.py): unit test added for utils

---
+++ tests/unit/test_utils.py
@@ -0,0 +1,25 @@
+import pytest
+from deepclassifier.utils import read_yaml
+from pathlib import Path
+from box import ConfigBox
+from ensure.main import EnsureError
+
+# TODO tox # will run the tox configuration
+
+# TODO pytest -v
+class Test_read_yaml:
+    yaml_files = ["tests\data\demo.yaml", "tests\data\empty.yaml"]
+
+    def test_read_yaml_empty(self):
+        with pytest.raises(ValueError):  # if it raises
+            read_yaml(Path(self.yaml_files[1]))
+
+    def test_read_yaml_out(self):
+        response = read_yaml(Path(self.yaml_files[0]))
+        assert isinstance(response, ConfigBox)
+
+    @pytest.mark.parametrize("path_to_yaml", yaml_files)
+    # @pytest.mark.parametrize("path_to_yaml", yaml_files)
+    def test_read_yaml_bad_type(self, path_to_yaml):
+        with pytest.raises(EnsureError):
+            read_yaml(path_to_yaml)
