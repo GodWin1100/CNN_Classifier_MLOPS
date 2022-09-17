59b7b5b2eca6e82207c7b164fe477a570e658e91
GodWin <shivamjpanchal1@gmail.com>
2022-09-18 03:51:07 +0530

    refactor(s01_daata_ingestion.py): run as main for dvc

--- src/DeepClassifier/pipeline/s01_data_ingestion.py
+++ src/DeepClassifier/pipeline/s01_data_ingestion.py
@@ -1,13 +1,24 @@
+from tkinter import CENTER
 from deepclassifier.components import DataIngestion
 from deepclassifier.config import ConfigurationManager
+from deepclassifier import logger
 
-try:
+STAGE_NAME = " Data Ingestion Stage"
+
+
+def main():
     config = ConfigurationManager()
     data_ingestion_config = config.get_data_ingestion_config()
     data_ingestion = DataIngestion(config=data_ingestion_config)
     data_ingestion.download_file()
     data_ingestion.unzip_clean()
-except Exception as e:
-    raise e
 
-print("hi")
+
+if __name__ == "__main__":
+    try:
+        logger.info(f" STAGE: {STAGE_NAME} STARTED ".center(100, "="))
+        main()
+        logger.info(f" STAGE: {STAGE_NAME} COMPLETED ".center(100, "="))
+    except Exception as e:
+        logger.exception(e)
+        raise e
