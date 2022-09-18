77826eb261c8adfde17cf9d778f2d2fe4699e3a4
GodWin <shivamjpanchal1@gmail.com>
2022-09-18 06:32:59 +0530

    refactor(test_utils): used raw stiring for path

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -8,7 +8,7 @@ from ensure.main import EnsureError
 
 # TODO pytest -v
 class Test_read_yaml:
-    yaml_files = ["tests\data\demo.yaml", "tests\data\empty.yaml"]
+    yaml_files = [r"tests\data\demo.yaml", r"tests\data\empty.yaml"]
 
     def test_read_yaml_empty(self):
         with pytest.raises(ValueError):  # if it raises
@@ -19,7 +19,6 @@ class Test_read_yaml:
         assert isinstance(response, ConfigBox)
 
     @pytest.mark.parametrize("path_to_yaml", yaml_files)
-    # @pytest.mark.parametrize("path_to_yaml", yaml_files)
     def test_read_yaml_bad_type(self, path_to_yaml):
         with pytest.raises(EnsureError):
             read_yaml(path_to_yaml)
