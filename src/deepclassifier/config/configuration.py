97efecd81fcf7dd94e6ce220008df5b8f9f516b6
GodWin <shivamjpanchal1@gmail.com>
2022-09-21 04:04:48 +0530

    feat(config): stage 2, 3, 4 configuration added

--- src/deepclassifier/config/configuration.py
+++ src/deepclassifier/config/configuration.py
@@ -1,6 +1,8 @@
+import os
+from pathlib import Path
 from deepclassifier.constants import CONFIG_FILE_PATH, PARAM_FILE_PATH
 from deepclassifier.utils import read_yaml, create_directories
-from deepclassifier.entity import DataIngestionConfig
+from deepclassifier.entity import DataIngestionConfig, PrepareBaseModelConfig, PrepareCallbacksConfig, TrainingConfig
 
 
 class ConfigurationManager:
@@ -19,3 +21,50 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir,
         )
         return data_ingestion_config
+
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        params = self.params
+        create_directories([config.root_dir])
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+            params_classes=params.CLASSES,
+            params_image_size=params.IMAGE_SIZE,
+            params_include_top=params.INCLUDE_TOP,
+            params_learning_rate=params.LEARNING_RATE,
+            params_weights=params.WEIGHTS,
+        )
+        return prepare_base_model_config
+
+    def get_prepare_callbacks_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        params = self.params
+        create_directories(
+            [Path(config.tensorboard_root_log_dir), Path(os.path.dirname(config.checkpoint_model_filepath))]
+        )
+        prepare_callbacks_config = PrepareCallbacksConfig(
+            root_dir=Path(config.root_dir),
+            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath=Path(config.checkpoint_model_filepath),
+        )
+        return prepare_callbacks_config
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "PetImages")
+        params = self.params
+        create_directories([Path(training.root_dir)])
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE,
+        )
+        return training_config
